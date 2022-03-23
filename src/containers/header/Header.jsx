322589ac835f4524c9bd5ec4e2ffcbf9968b4f43
JoshuaMilesSD <joshmiles2010@gmail.com>
2022-03-22 18:07:07 -0700

    trying to update to github

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className='gpt3__header section__padding'>
+     {//stopping at 1:07:46 // }
+    </div>
+  )
+}
+
+export default Header
