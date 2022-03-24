af5e34b2f8eeddb72bf6719855a178b2496bcb6d
JoshuaMilesSD <joshmiles2010@gmail.com>
2022-03-23 19:05:53 -0700

    updating for 3/23/2022

--- src/containers/what/What.jsx
+++ src/containers/what/What.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './what.css';
+import Feature from '../features/Features';
 
 
 const What = () => {
   return (
-    <div>What</div>
+    < div className='gpt3__whatgpt3 section__margin' id="wgpt3">
+
+    <div className='gpt3__whatgpt3-feature'>
+      <Feature />
+    </div>
+
+    <div className='gpt3__whatgpt3-heading'>
+      <h1 className='gradient__text'>
+        The possibilities are beyond your imagination</h1>
+        <p> Explore the Library</p>
+    </div>
+    <div className='gpt3__whatgpt3-container'>
+      <Feature />
+      <Feature />
+      <Feature />
+    </div>
+    </div>
   )
 }
 
