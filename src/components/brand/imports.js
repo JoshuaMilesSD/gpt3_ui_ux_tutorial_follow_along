0103e439620b0b4e231e730efbc75e66058f95d5
JoshuaMilesSD <joshmiles2010@gmail.com>
2022-04-05 17:09:02 -0700

    update

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/imgs/google.png';
+import slack from '../../assets/imgs/slack.png';
+import atlassian from '../../assets/imgs/atlassian.png';
+import dropbox from '../../assets/imgs/dropbox.png';
+import shopify from '../../assets/imgs/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify,
+}
